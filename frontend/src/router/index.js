e079d945ca0015d06fe2d89aa32a80225ba14e3c
Stonuv <stonuv@gmail.com>
2026-06-15 02:17:28 +0300

    updated router/index.js

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -1,12 +1,18 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import MastersPage from '../views/MastersPage.vue'
-import BookingPage from '../views/BookingPage.vue'
-import LoginPage   from '../views/LoginPage.vue'
+import MastersPage     from '../views/MastersPage.vue'
+import BookingPage     from '../views/BookingPage.vue'
+import LoginPage       from '../views/LoginPage.vue'
+import ProfilePage     from '../views/ProfilePage.vue'
+import MasterDashboard from '../views/MasterDashboard.vue'
+import AdminPanel      from '../views/AdminPanel.vue'
 
 const routes = [
-  { path: '/',            component: MastersPage },
-  { path: '/masters/:id', component: BookingPage },
-  { path: '/login',       component: LoginPage   },
+  { path: '/',            component: MastersPage     },
+  { path: '/masters/:id', component: BookingPage     },
+  { path: '/login',       component: LoginPage       },
+  { path: '/profile',     component: ProfilePage     },
+  { path: '/dashboard',   component: MasterDashboard },
+  { path: '/admin',       component: AdminPanel      },
 ]
 
 export default createRouter({
